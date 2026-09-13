0b655ff6588768c6294baee292b178ebac229fb0
vladkavaceslavovna <v-shalayeva07@mail.ru>
2026-09-13 20:17:31 +0500

    Task4

---
+++ Tyuiu.ShalaevaVV.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿namespace Tyuiu.ShalaevaVV.Sprint0.Task4.V0.Lib;
+
+public class DataService
+{
+    //Пример линейной структуры
+    public static int Addition(int a, int b)
+    {
+        return a + b;
+    }
+    public static int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+    public static int Multiplication(int a, int b)
+    {
+        return a * b;
+    }
+    public static int Division(int a, int b)
+    {
+        return a / b;
+    }
+}
---
+++ Tyuiu.ShalaevaVV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.ShalaevaVV.Sprint0.Task4.V0.Lib;
+//Пример линейной структуры программы
+// Вызов метода сложения Addiction
+Console.WriteLine(DataService.Addition(1, 5));
+//Вызов метода вычитания Subtraction
+Console.WriteLine(DataService.Subtraction(15, 5));
+//Вызом метода умножения Multiplication
+Console.WriteLine(DataService.Multiplication(10, 10));
+//Вызов метода деления Division
+Console.WriteLine(DataService.Division(5, 5));
+Console.ReadKey();
